29a52be5e23b7f2d991a58b1db5c93de3a6995c1
YangYang <ss880309@gmail.com>
2021-01-28 15:21:46 +0800

    add log

--- controllers/asset.js
+++ controllers/asset.js
@@ -5,12 +5,14 @@ const { loadImageList, loadVideoList } = require('../services/asset');
 router.get('/images/:keywords', async(req, res, next) => {
     const keywords = req.params.keywords;
     const data = await loadImageList(keywords);
+    console.log('images data: ',data);
     res.json(data);
 });
 
 router.get('/videos/:keywords', async(req, res, next) => {
     const keywords = req.params.keywords;
     const data = await loadVideoList(keywords);
+    console.log('videos data: ',data);
     res.json(data);
 });
 
