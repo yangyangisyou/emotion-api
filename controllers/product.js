50c37477daefc7b74c1f7593ee4cfa66864244f4
YangYang <ss880309@gmail.com>
2021-02-03 10:38:58 +0800

    fix import

--- controllers/product.js
+++ controllers/product.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router();
-const { getTitleList, getProductList, createProduct, uploadImage } = require('../services/product');
+const { getTitleList, getProductList, createProduct, uploadImage, updateProduct } = require('../services/product');
 
 router.get('/hotTitle', async(req, res, next) => {
     const data = await getTitleList();
