1d827d85b67bd8d8d37b62fe9afdedc4ae63da1c
yangyangisyou <ss880309@gmail.com>
2021-02-06 21:34:59 +0800

    Fix link

--- config/setting.js
+++ config/setting.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 8000;
 const PIXABAY_PATH = 'https://pixabay.com/api';
 const PIXABAY_API_KEY = process.env.PIXABAY_API_KEY;
 
-const NEWS_PATH = 'http://api.mediastack.com/v1/';
+const NEWS_PATH = 'http://api.mediastack.com/v1';
 const NEWS_API_KEY = process.env.NEWS_KEY;
 
 module.exports = {
