60ea932301ca53c78b7838e9b630f739406c6dff
yangyangisyou <ss880309@gmail.com>
2021-02-07 14:10:34 +0800

    change query api

--- services/product.js
+++ services/product.js
@@ -82,11 +82,11 @@ async function getProductList(productType) {
     // const data = await dynamoDB.batchGetItems(queryParams);
     const queryParams = {
       TableName: 'products',
-      FilterExpression: `#status = :status_val`,
+      FilterExpression: `#productType = :productType`,
       ExpressionAttributeNames: {
         "#productType": "productType",
     },
-    ExpressionAttributeValues: { ":status_val": productType }
+    ExpressionAttributeValues: { ":productType": productType }
     };
     console.log('queryParams ->', queryParams);
     console.log('queryParams ->', JSON.stringify(queryParams));
