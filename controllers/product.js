86ef4db5f574460426a55d2d5fcb239e6eaa1996
yangyangisyou <ss880309@gmail.com>
2021-02-21 13:55:40 +0800

    Add filter

--- controllers/product.js
+++ controllers/product.js
@@ -6,8 +6,10 @@ router.get('/list/:productType', async(req, res, next) => {
   const productType = req.params.productType;
   const result = await getProductList(productType);
   if(result.code === 200) {
-    console.log('result.data ',result.data);
-    const filteredData = result.data.filter(eachData => eachData.imageName);
+    const filteredData = {
+      ...result.data,
+      Items: result.data.Items.filter(eachData => eachData.imageName)
+    };
     res.json({
       success: true,
       message: 'load-product-list-success',
